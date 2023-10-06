b0d2cc75e40b20ff835c8ca884c0d32867fb3a98
Mounika-reddy-mukku <mounikareddtmukku@gmail.com>
2023-10-06 11:01:47 +0530

    git push

---
+++ ar.js
@@ -0,0 +1,31 @@
+var a=10;
+var b=10.5;
+var c="Mounika";
+var d='30';
+var e=true
+var f=false
+var g;
+ //+operator and concatination
+ console.log(a+b);
+ console.log(a+c);
+ console.log(a+e);
+ console.log(a+d);
+ console.log(a+f);
+ console.log(a+g);
+ console.log(c+e);
+ /*20.5
+10Mounika
+11
+1030
+10
+NaN
+Mounikatrue*/
+
+//*operator
+console.log(a*b);
+console.log(a*c);
+console.log(a*g);
+/*105
+NaN
+NaN*/
+
---
+++ terinary.js
@@ -0,0 +1,2 @@
+var sal=45000;
+sal>75000? console.log("Eligible") : console.log("Not eligible")
